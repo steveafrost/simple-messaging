1feb893f77a5a9dbefad0bf934948905d86aefa2
Steve Frost <hello@steveafrost.com>
2017-07-15 17:17:59 -0400

    generated single static template for proof of concept

--- js/templatesModule.js
+++ js/templatesModule.js
@@ -11,6 +11,7 @@
       this.$templateField = $('#templateField');
       this.$companyField = $('#companyField');
       this.$guestField = $('#guestField');
+      this.$messageArea = $('#message');
     },
     bindEvents: function() {
       this.$form.submit(this.populateTemplate.bind(this));
@@ -33,7 +34,7 @@
           company = JSON.parse(this.$companyField.val()),
           guest = JSON.parse(this.$guestField.val());
 
-
+      this.$messageArea.text(`{time of day here} ${guest.firstName}, and welcome to ${company.company}! Room ${guest.reservation.roomNumber} is now ready for you. Enjoy your stay, and let us know if you need anything!`)
     }
   }
 
